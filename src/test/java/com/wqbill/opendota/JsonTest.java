ba93b595be2d25a4f892d4cfd974e871a7b89bfc
wq <wqbill@163.com>
2019-12-16 17:48:01 +0800

    work

--- src/test/java/com/wqbill/opendota/JsonTest.java
+++ src/test/java/com/wqbill/opendota/JsonTest.java
@@ -4,35 +4,29 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.*;
 import com.wqbill.opendota.json.Field;
-import javafx.util.Pair;
-import lombok.ToString;
-import org.junit.Test;
+import com.wqbill.opendota.util.FieldUtils;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
-import static com.fasterxml.jackson.databind.node.JsonNodeType.*;
+import java.util.*;
 
 public class JsonTest {
     public static void main(String[] args) throws IOException {
         ClassPathResource classPathResource = new ClassPathResource("/IDOTA2Match_570/GetMatchDetails.json");
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(classPathResource.getInputStream());
-        Stack<Pair<JsonNode, Integer>> stack = new Stack<>();
-        stack.push(new Pair<>(root, 0));
-        bfs(stack);
+        Queue<JsonNode> queue = new LinkedList<>();
+        queue.offer(root);
+        traversal(queue);
     }
 
-    private static void bfs(Stack<Pair<JsonNode, Integer>> stack) {
-        if (!stack.isEmpty()) {
-            Pair<JsonNode, Integer> pair = stack.pop();
-            JsonNode node = pair.getKey();
-            Integer depth = pair.getValue();
+    private static void traversal(Queue<JsonNode> queue) {
+        if (!queue.isEmpty()) {
+            JsonNode node = queue.poll();
+            System.out.println("start###############");
+            // 最顶层不是array
             if (node instanceof ObjectNode) {
                 // 每个object生成一个class
                 ObjectNode on = (ObjectNode) node;
@@ -41,60 +35,84 @@ public class JsonTest {
                     Field field = new Field();
                     field.setOriginalName(x.getKey());
                     fields.add(field);
-                    System.out.println(x.getKey() + "\t" + depth + "\t" + x.getValue().getNodeType());
-                    if (x.getValue().getNodeType() == NUMBER) {
-                        String type = "Double";
-                        switch (x.getValue().numberType()) {
-                            case INT:
-                                type = Integer.class.getSimpleName();
-                                break;
-                            case LONG:
-                                type = Long.class.getSimpleName();
-                                break;
-                            case BIG_INTEGER:
-                                type = BigInteger.class.getSimpleName();
-                                break;
-                            case FLOAT:
-                                type = Float.class.getSimpleName();
-                                break;
-                            case DOUBLE:
-                                type = Double.class.getSimpleName();
-                                break;
-                            case BIG_DECIMAL:
-                                type = BigDecimal.class.getSimpleName();
-                                break;
-                        }
-                        field.setType(type);
-                    }else if(x.getValue().getNodeType()==ARRAY){
-                        field.setType("List<subType>");
-                    }else if(x.getValue().getNodeType()==BOOLEAN){
-                        field.setType(Boolean.class.getSimpleName());
-                    }else if(x.getValue().getNodeType()==OBJECT){
-                        field.setType("subType");
-                    } else if(x.getValue().getNodeType()==STRING){
-                        field.setType(String.class.getSimpleName());
+                    JsonNode fieldNode = x.getValue();
+                    System.out.println(x.getKey() + "\t" + fieldNode.getNodeType());
+                    switch (fieldNode.getNodeType()) {
+                        case ARRAY:
+                            field.setType("List<subType>");
+                            System.out.println("@@@@@@@@@@@@@@@@@@");
+                            ArrayNode an = (ArrayNode) fieldNode;
+                            int count = 0;
+                            while (an.size() > 0 && an.get(0) instanceof ArrayNode) {
+                                an = (ArrayNode) an.get(0);
+                                count++;
+                            }
+                            String subType;
+                            if (an.size() > 0) {
+                                JsonNode firstSub = an.get(0);
+                                queue.offer(firstSub);
+                                subType = FieldUtils.capitalFirst(FieldUtils.singular(x.getKey()));
+                            } else {
+                                subType = "Object";
+                            }
+                            StringBuilder sb1 = new StringBuilder();
+                            StringBuilder sb2 = new StringBuilder();
+                            for (int i = 0; i <= count; i++) {
+                                sb1.append("List<");
+                                sb2.append(">");
+                            }
+                            String type = sb1.append(subType).append(sb2).toString();
+                            field.setType(type);
+                            break;
+                        case BINARY:
+                            break;
+                        case BOOLEAN:
+                            field.setType(Boolean.class.getSimpleName());
+                            break;
+                        case MISSING:
+                            break;
+                        case NULL:
+                            break;
+                        case NUMBER:
+                            type = null;
+                            switch (fieldNode.numberType()) {
+                                case INT:
+                                    type = Integer.class.getSimpleName();
+                                    break;
+                                case LONG:
+                                    type = Long.class.getSimpleName();
+                                    break;
+                                case BIG_INTEGER:
+                                    type = BigInteger.class.getSimpleName();
+                                    break;
+                                case FLOAT:
+                                    type = Float.class.getSimpleName();
+                                    break;
+                                case DOUBLE:
+                                    type = Double.class.getSimpleName();
+                                    break;
+                                case BIG_DECIMAL:
+                                    type = BigDecimal.class.getSimpleName();
+                                    break;
+                            }
+                            field.setType(type);
+                            break;
+                        case OBJECT:
+                            field.setType(FieldUtils.capitalFirst(FieldUtils.singular(x.getKey())));
+                            queue.offer(x.getValue());
+                            break;
+                        case POJO:
+                            break;
+                        case STRING:
+                            field.setType(String.class.getSimpleName());
+                            break;
                     }
                     System.out.println(field.getType());
                     fields.add(field);
-                    stack.push(new Pair<>(x.getValue(), depth + 1));
                 });
-            } else if (node instanceof ArrayNode) {
-                ArrayNode an = (ArrayNode) node;
-                if (an.size() > 0) {
-                    JsonNode firstSub = an.get(0);
-                    stack.push(new Pair<>(firstSub, depth));
-                }
-            } else if (node instanceof ValueNode) {
-            } else {
-                System.out.println("unknown situation");
             }
-            bfs(stack);
+            System.out.println("end###############");
+            traversal(queue);
         }
-    }
-
-    @Test
-    public void test(){
-        System.out.println(BigDecimal.class.getSimpleName());
-        System.out.println(123);
     }
 }
