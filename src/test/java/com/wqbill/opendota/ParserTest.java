f7d915f75a7ac547d19f06c9cd6a087c3a2e096a
wq <wqbill@163.com>
2019-12-18 16:23:33 +0800

    prepare to parse dem

---
+++ src/test/java/com/wqbill/opendota/ParserTest.java
@@ -0,0 +1,15 @@
+package com.wqbill.opendota;
+
+public class ParserTest {
+    public static void main(String[] args) {
+
+    }
+
+    public void test(){
+        String replayUrl = "http://replay<cluster>.valve.net/570/<match_id>_<replay_salt>.dem.bz2";
+        int cluster = 227;
+        long match_id = 5130564964L;
+        // 找不到
+        int replay_salt = 1323404802;
+    }
+}
